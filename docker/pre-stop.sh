7b0242ffc6c3f0141a3a494e14772dd404d909a3
harikiran nayak <hari@streamsets.com>
2017-06-26 14:52:52 -0700

    Fix Docker file and script files to run pre-stop script on pod termination

--- docker/pre-stop.sh
+++ docker/pre-stop.sh
@@ -6,9 +6,9 @@ PASSWORD=$(cat /var/lib/sdc-resources/dpmpassword)
 SDC_ID=$(cat /data/sdc.id)
 
 # login to security app
-curl -X POST -d "{\"userName\":\"${USERNAME}\", \"password\": \"${PASSWORD}\"}" ${URL}/security/public-rest/v1/authentication/login --header "Content-Type:application/json" --header "X-Requested-By:SDC" -c cookie.txt
+curl -X POST -d "{\"userName\":\"${USERNAME}\", \"password\": \"${PASSWORD}\"}" ${URL}/security/public-rest/v1/authentication/login --header "Content-Type:application/json" --header "X-Requested-By:SDC" -c /tmp/cookie.txt
 # generate auth token from security app
-sessionToken=`sed -n '/SS-SSO-LOGIN/p' cookie.txt | perl -lane 'print $F[$#F]'`
+sessionToken=`sed -n '/SS-SSO-LOGIN/p' /tmp/cookie.txt | perl -lane 'print $F[$#F]'`
 
 # Call DPM rest APIs to de-register sdc from DPM
 curl -X POST -d "[ \"${SDC_ID}\" ]" ${URL}/security/rest/v1/organization/${ORG}/components/deactivate --header "Content-Type:application/json" --header "X-Requested-By:SDC" --header "X-SS-REST-CALL:true" --header "X-SS-User-Auth-Token:$sessionToken"
